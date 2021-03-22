0a853ffaccc3cdebd84bfb58942b2316041f1a55
atrox3d <atrox3d@gmail.com>
2021-03-23 00:22:17 +0100

    templating posts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import util.network, util.api as api
+import json
 
 app = Flask(__name__)
 
@@ -8,6 +9,12 @@ app = Flask(__name__)
 def home():
     posts = api.get_posts()
     return render_template("index.html", posts=posts)
+
+
+@app.route('/post/<post_id>')
+def read_post(post_id):
+    post = [post for post in api.get_posts() if post["id"] == int(post_id)][0]
+    return render_template("post.html", post=post)
 
 
 if __name__ == "__main__":
